407c00315556de42a40f3ac40e37287d4f725540
Guilherme <guilhermehas@hotmail.com>
2023-11-05 10:36:23 +0100

    added overlay

--- flake.nix
+++ flake.nix
@@ -8,24 +8,31 @@
   };
 
   outputs = { self, flake-utils, flake-compat, nixpkgs }:
-    let overlays = import ./nix/overlay.nix nixpkgs.lib; in
+    let
+        linear-algebra-overlay = prev: next: with next.agdaPackages;
+          {
+            agdaPackages = next.agdaPackages // {
+                linear-algebra = mkDerivation {
+                pname = "agda-dimensional-stdlib";
+                version = "1.0.0";
+                src = ./.;
+                everythingFile = "src/EverythingUseful.agda";
+                buildInputs = [ standard-library ];
+                LC_ALL = "en_US.UTF-8";
+                nativeBuildInputs = [ prev.glibcLocales ];
+                meta = {};
+              };
+            };
+          };
+        overlays = import ./nix/overlay.nix nixpkgs.lib ++ [ linear-algebra-overlay ];
+    in
     flake-utils.lib.eachDefaultSystem (system:
         let pkgs = import nixpkgs { inherit system overlays; };
         agda-all = pkgs.agda.withPackages (p: with p; [ standard-library ]);
+        linear-algebra = pkgs.agdaPackages.linear-algebra;
     in rec {
       packages = {
-        inherit agda-all;
-        linear-algebra = with pkgs; with agdaPackages;
-          agdaPackages.mkDerivation {
-            pname = "agda-dimensional-stdlib";
-            version = "1.0.0";
-            src = ./.;
-            everythingFile = "src/EverythingUseful.agda";
-            buildInputs = [ standard-library ];
-            LC_ALL = "en_US.UTF-8";
-            nativeBuildInputs = [ glibcLocales ];
-            meta = {};
-          };
+        inherit agda-all linear-algebra;
       };
       defaultPackage = packages.linear-algebra;
     });
